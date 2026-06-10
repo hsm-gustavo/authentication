bd61a4a8f0f3f9f6941c228a07543c14382c02bc
Gustavo Malaquias <gustavo.hs.malaquias@gmail.com>
2026-06-10 15:16:41 -0300

    feat: add util to calculate days since today from another date

--- shared/date/date.go
+++ shared/date/date.go
@@ -5,3 +5,7 @@ import "time"
 func IsExpired(expiresAt time.Time) bool {
 	return time.Now().After(expiresAt)
 }
+
+func IsDaysOld(test time.Time) int {
+	return int(time.Since(test).Abs().Hours()/24)
+}
